f6ded43d470d989e116ce77bd7397b7f441dab0e
Mir08 <78381939+Mir08@users.noreply.github.com>
2021-06-04 14:44:22 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -41,3 +41,83 @@ function new_image(get_image)
 
     });
 }
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+    keyPressed = e.keyCode;
+    console.log(keyPressed);
+    
+    if (e.shiftkey == true && keyPressed == '80') 
+    {
+    console.log("p and shiftkey pressed together");
+    block_image_width = block_image_width + 10;
+    block_image_height = block_image_height + 10;
+    document.getElementById("current_width").innerHTML = block_image_width;
+    document.getElementById("current_height").innerHTML = block_image_height;
+    }
+    
+    if (e.shiftkey && keyPressed == '77') 
+    {
+    console.log("m and shiftkey pressed together");
+    block_image_width = block_image_width - 10;
+    block_image_height = block_image_height - 10;
+    document.getElementById("current_width").innerHTML = block_image_width;
+    document.getElementById("current_height").innerHTML = block_image_height;
+    }
+
+    if (keyPressed == '38')
+    {
+        up();
+        console.log("up");
+    }
+
+    if (keyPressed == '40')
+    {
+        down();
+        console.log("down");
+    }
+
+    if (keyPressed == '37')
+    {
+        left();
+        console.log("left");
+    }
+
+    if (keyPressed == '39')
+    {
+        right();
+        console.log("right");
+    }
+
+    if (keyPressed == '70')
+    {
+        new_image('ironman_face.png');
+        console.log("f");
+    }
+
+    if (keyPressed == '66')
+    {
+        new_image('spiderman_body.png');
+        console.log("b");
+    }
+
+    if (keyPressed == '76')
+    {
+        new_image('hulk_legs.png');
+        console.log("l");
+    }
+
+    if (keyPressed == '82')
+    {
+        new_image('thor_right_hand');
+        console.log("r");
+    }
+
+    if (keyPressed == '72')
+    {
+        new_image('captain_america_left_hand.png');
+        console.log("h");
+    }
+}
